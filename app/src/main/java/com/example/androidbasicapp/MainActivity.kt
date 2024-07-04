88dbb6a96394ed21bc2e653a932197af8dbd49cb
Mohan-A-Pepul <mohan17122k@gmail.com>
2024-07-04 16:57:01 +0530

    color basic

--- app/src/main/java/com/example/androidbasicapp/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.androidbasicapp
 
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -18,12 +22,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidbasicapp.ui.activities.SampleClassViewModel
 import com.example.androidbasicapp.ui.theme.AndroidBasicAppTheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
 
@@ -43,16 +50,36 @@ class MainActivity : ComponentActivity() {
 
         println("MainActivity >>> onCreate")
 
+        val conf = resources.configuration
+        val local = "ta"
+        val locale = Locale(local)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            conf.setLocale(locale)
+            conf.setLayoutDirection(locale)
+            resources.updateConfiguration(conf,resources.displayMetrics) /**Deprecated**/
+        }
+        else{
+            conf.setLocale(locale)
+            conf.setLayoutDirection(locale)
+            createConfigurationContext(conf)
+        }
+
         setContent {
-            AndroidBasicAppTheme {
-                Surface(modifier = Modifier.fillMaxSize(),color = viewModel.color.value) {
+
+                Surface(modifier = Modifier.fillMaxSize(),color = colorResource(id = viewModel.color.value)) {
+
+                    Image(painter = painterResource(id = R.drawable.res_hash), contentDescription = "",
+                        modifier = Modifier.fillMaxWidth())
                     Button(onClick = {
-                        viewModel.changeBackgroundColor(Color.Blue)
-                    },  modifier = Modifier.requiredHeight(120.dp).requiredWidth(120.dp).padding(2.dp)) {
-                        Text(text = "Click me", color = Color.Blue)
+                        viewModel.changeBackgroundColor(R.color.teal_200)
+                    },  modifier = Modifier
+                        .requiredHeight(120.dp)
+                        .requiredWidth(120.dp)
+                        .padding(2.dp)
+                    ) {
+                        Text(text = getString(R.string.hello), color = Color.Blue, maxLines = 1, softWrap = true, minLines = 1)
                     }
                 }
-            }
         }
     }
 
--- app/src/main/java/com/example/androidbasicapp/ui/activities/SampleClassViewModel.kt
+++ app/src/main/java/com/example/androidbasicapp/ui/activities/SampleClassViewModel.kt
@@ -3,15 +3,16 @@ package com.example.androidbasicapp.ui.activities
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import com.example.androidbasicapp.R
 
 class SampleClassViewModel(
     private val hello:String
 ) : ViewModel() {
 
-    private val backgroundColor =  mutableStateOf(Color.White)
+    private val backgroundColor =  mutableStateOf(R.color.white)
     val color = backgroundColor
 
-    fun changeBackgroundColor(color: Color) {
+    fun changeBackgroundColor(color: Int) {
         backgroundColor.value = color
     }
 
